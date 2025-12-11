277f78963cf7b5054af2926277d661bd492b7cfd
Vusi <vnwedo@gmail.com>
2025-12-11 20:49:50 +0200

    try to move the Items thing up

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -286,7 +286,7 @@ function ProductList({ onHomeClick }) {
 
   const badgeStyle = {
     position: 'absolute',
-    top: '80%',
+    top: '20%',
     left: '50%',
     // Added a small pixel offset to correct visual misalignment
     transform: 'translate(calc(-50% + 1px), calc(-50% + 2px))', 
